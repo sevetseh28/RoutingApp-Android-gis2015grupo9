6e9a3087de7618326ca9a2075c3e3add47dd160e
sevetseh28 <hernaner28@gmail.com>
2015-10-15 18:33:35 -0300

    initial commit

---
+++ app/src/main/java/com/example/hernan/examplegis/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.hernan.examplegis;
+
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.esri.core.geometry.Point;
+import com.esri.core.tasks.geocode.Locator;
+import com.esri.core.tasks.geocode.LocatorFindParameters;
+import com.esri.core.tasks.geocode.LocatorGeocodeResult;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mSearchEditText;
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Esto es para la busqueda, no se muy bien que es lo que hace
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) findViewById(R.id.searchView);
+        SearchableInfo info = searchManager.getSearchableInfo(getComponentName());
+        searchView.setSearchableInfo(info);
+
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/hernan/examplegis/SearchableActivity.java
@@ -0,0 +1,135 @@
+package com.example.hernan.examplegis;
+
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.esri.core.tasks.geocode.Locator;
+import com.esri.core.tasks.geocode.LocatorFindParameters;
+import com.esri.core.tasks.geocode.LocatorGeocodeResult;
+
+import java.util.List;
+
+public class SearchableActivity extends ListActivity {
+    public Context getContext() {
+        return this.getContext();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchable);
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //doMySearch(query);
+            executeLocatorTask(query);
+        }
+    }
+
+    /**
+     * Called from search_layout.xml when user presses Search button
+     *
+     * @param view
+     */
+/*
+    public void onSearchButtonClicked(View view) {
+        // Hide virtual keyboard
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+        // obtain address and execute locator task
+        String address = mSearchEditText.getText().toString();
+        executeLocatorTask(address);
+
+    }
+*/
+
+
+    /**
+     * Set up the search parameters and execute the Locator task.
+     *
+     * @param address
+     */
+    private void executeLocatorTask(String address) {
+        // Create Locator parameters from single line address string
+        LocatorFindParameters findParams = new LocatorFindParameters(address);
+
+
+        // Execute async task to find the address
+        new LocatorAsyncTask().execute(findParams);
+    }
+
+
+    private class LocatorAsyncTask extends AsyncTask<LocatorFindParameters, Void, List<LocatorGeocodeResult>> {
+        private Exception mException;
+
+        public LocatorAsyncTask() {
+        }
+
+        @Override
+        protected List<LocatorGeocodeResult> doInBackground(
+                LocatorFindParameters... params) {
+            // Perform routing request on background thread
+            mException = null;
+            List<LocatorGeocodeResult> results = null;
+
+            // Create locator using default online geocoding service and tell it
+            // to find the given address
+            Locator locator = Locator.createOnlineLocator();
+            try {
+                results = locator.find(params[0]);
+            } catch (Exception e) {
+                mException = e;
+            }
+            return results;
+        }
+
+        @Override
+        protected void onPostExecute(List<LocatorGeocodeResult> result) {
+            // Display results on UI thread
+            if (mException != null) {
+                //Log.w(TAG, "LocatorSyncTask failed with:");
+                mException.printStackTrace();
+                Toast.makeText(SearchableActivity.this, getString(R.string.addressSearchFailed), Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            if (result.size() == 0) {
+                Toast.makeText(SearchableActivity.this,
+                        getString(R.string.noResultsFound), Toast.LENGTH_LONG).show();
+            } else {
+                // Use first result in the list
+                ArrayAdapter<String> itemsAdapter =
+                        new ArrayAdapter<String>(SearchableActivity.this, android.R.layout.simple_list_item_1);
+
+                for (int i=0; i<result.size(); i++ ){
+                    LocatorGeocodeResult geocodeResult = result.get(i);
+                    itemsAdapter.add(geocodeResult.getAddress());
+                }
+                ListView listView = (ListView) getListView();
+                listView.setAdapter(itemsAdapter);
+                //LocatorGeocodeResult geocodeResult = result.get(0);
+                // crear alerta
+                //Toast.makeText(SearchableActivity.this, geocodeResult.getAddress(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+
+}
